68bc1a16a569305debbab27138e72b4dbeb6e87c
Matthew Brett <matthew.brett@gmail.com>
2018-02-09 22:28:49 +0000

    BF: fix multibuild to use env_vars correctly
    Then use CPPFLAGS to set flags we need.

--- env_vars.sh
+++ env_vars.sh
@@ -2,4 +2,4 @@
 
 # Fix an issue with MAX_SIZE definition.
 # ref: https://stackoverflow.com/a/30472875
-export CFLAGS="-D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS"
+export CPPFLAGS="-D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS"
