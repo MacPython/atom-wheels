2f03c80fedf732b05ca11a2e24a564131015d2b6
Matthew Brett <matthew.brett@gmail.com>
2018-02-09 16:47:58 +0000

    Try putting CFLAGS into env_vars

---
+++ env_vars.sh
@@ -0,0 +1,5 @@
+# Set environment variables for builds
+
+# Fix an issue with MAX_SIZE definition.
+# ref: https://stackoverflow.com/a/30472875
+export CFLAGS="-D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS"
